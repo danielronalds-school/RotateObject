241360f80de06bf591de5a5e14b07c9e6971f944
danielronalds <danielronalds@mmc.school.nz>
2020-06-26 15:07:15 +1200

    Big Push, finished spaceship and started creating the missile

--- RotateObject/Form1.Designer.cs
+++ RotateObject/Form1.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Canvas = new System.Windows.Forms.Panel();
+            this.tmrSpaceship = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // Canvas
@@ -39,6 +41,13 @@
             this.Canvas.Size = new System.Drawing.Size(716, 522);
             this.Canvas.TabIndex = 0;
             this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // tmrSpaceship
+            // 
+            this.tmrSpaceship.Enabled = true;
+            this.tmrSpaceship.Interval = 1;
+            this.tmrSpaceship.Tick += new System.EventHandler(this.tmrSpaceship_Tick);
             // 
             // Form1
             // 
@@ -48,6 +57,9 @@
             this.Controls.Add(this.Canvas);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
 
         }
@@ -55,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.Timer tmrSpaceship;
     }
 }
 
--- RotateObject/Form1.cs
+++ RotateObject/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+
 
 namespace RotateObject
 {
@@ -15,9 +17,14 @@ namespace RotateObject
         Graphics g;
         Spaceship spaceship = new Spaceship();
 
+        bool turnLeft;
+        bool turnRight;
+
         public Form1()
         {
             InitializeComponent();
+            typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic, null, Canvas, new object[] { true });
+
         }
 
         private void Canvas_Paint(object sender, PaintEventArgs e)
@@ -25,6 +32,37 @@ namespace RotateObject
             g = e.Graphics;
             // Drawing the spaceship
             spaceship.drawSpaceship(g);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            spaceship.moveSpaceship(e.X, e.Y);
+            Console.WriteLine("Mouse Moved");
+        }
+
+        private void tmrSpaceship_Tick(object sender, EventArgs e)
+        {
+            if (turnRight)
+            {
+                spaceship.rotationAngle += 5;
+            }
+            if (turnLeft)
+            { 
+                spaceship.rotationAngle -= 5;
+            }
+        Canvas.Invalidate();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { turnLeft = true; }
+            if (e.KeyData == Keys.Right) { turnRight = true; }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { turnLeft = false; }
+            if (e.KeyData == Keys.Right) { turnRight = false; }
         }
     }
 }
